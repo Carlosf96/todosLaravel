dfaf0d9f288b16cbfb2194144eb51a15988da58e
Carlosf96 <carlos.f.roque96@gmail.com>
2023-04-27 19:52:08 -0500

    Continue messing around with tailwind custom styling on searchbar and card components

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div class='bg-gray-50 border border-gray-200 rounded p-6'>
+<div class='bg-gray-70 border border-gray-200 rounded p-6 hover:shadow-gray-600'>
     {{$slot}}
 </div>
--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,3 +1,20 @@
-<form>
-    <input type="text" name="todo" class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:outline-none focus:ring focus:ring-red-300 hover:shadow" />
+<form action="">
+    <div class="relative border-2 border-gray-100 m-4 rounded-lg">
+        <input
+        type="text"
+        name="todo"
+        class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:outline-none focus:ring focus:ring-red-300 hover:shadow"
+        placeholder="Add new todo"
+        />
+        <div class="absolute top-2 right-2">
+        <button 
+            type="submit"
+            class="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-600"
+        >
+            Add
+        </button>
+
+        </div>
+
+    </div>
 </form>
